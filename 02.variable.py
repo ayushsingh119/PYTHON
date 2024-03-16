e9dabfafc5608002567c1b085843ab16900010b3
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-16 22:44:32 +0530

    Add files via upload

---
+++ 02.variable.py
@@ -0,0 +1,23 @@
+# variables: a container for a value. behaves as the value that it contains.
+
+# first_name = "supernova"
+# last_name = "alive"
+# full_name = first_name +" "+ last_name
+# print(full_name)
+# print(type(full_name))
+
+
+# age = 19
+# age = age + 1
+# print(age)
+# print(type(age))
+# print("Your age is: " + str(age))
+
+# height = 250.5
+# print("Your height is: " + str(height))
+# print(type(height))
+
+human = True
+print(human)
+print(type(human))
+print("Are you a human: " + str(human))
