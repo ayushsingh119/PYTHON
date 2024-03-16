46bd01a74ef6977be82c3d6c4669a83a21972c0d
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-16 22:44:47 +0530

    Add files via upload

---
+++ 03.multiple_assignment.py
@@ -0,0 +1,28 @@
+# multiple assignment: allows us to assign variables at the same time in one line of code
+
+# name = "supernova"
+# age = 21
+# attractive = True
+# print(name)
+# print(age)
+# print(attractive)
+
+# name, age, attractive = "supernova" , 19, True
+# print(name)
+# print(age)
+# print(attractive)
+
+# spongebob = 30
+# patrick = 30
+# sandy = 30
+# sam = 30
+# print(spongebob)
+# print(patrick)
+# print(sandy)
+# print(sam)
+
+spongebob = patrick = sandy = sam = 30
+print(spongebob)
+print(patrick)
+print(sandy)
+print(sam)
