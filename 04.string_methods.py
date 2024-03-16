eec35dbf5fec730baf31571d23406b8b7a6a75fe
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-16 22:45:02 +0530

    Add files via upload

---
+++ 04.string_methods.py
@@ -0,0 +1,11 @@
+name = "supernova"
+print(len(name)) # it will help to know that what is the length of the name is
+print(name.find("s")) # it will help to know that on which index the letter s is in a string
+print(name.capitalize()) # it will help to capitalize the first letter of the word
+print(name.upper()) # it will help to print name in uppercase
+print(name.lower()) # it will help to print name in lower case
+print(name.isdigit()) # it will help to check tha name is digit or not if it is not then it will print False
+print(name.isalpha()) # it will help to check the name is alpha are not
+print(name.count("a")) # it will help to check that how many time a has come in the string
+print(name.replace("a","l")) # it will help to replace the letter with other letter in the string
+print(name*3) # it will help to print the string for multiple time according to your given value
