bb67f5b3d3ec50caea9d62faec2f79748684a672
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-18 22:17:24 +0530

    Add files via upload

---
+++ 06.user_input.py
@@ -0,0 +1,10 @@
+name = input("What is your name: ")
+age = int(input("how old are you:"))  # here we are making it int because by default it is string so we make it int for next process can be done of age = age + 1
+height = float(input("How tall are you: "))
+
+print("Hello " + name)
+print("I am " + str(age) + " years old!")
+print("I am " + str(height) + " cm tall")
+
+
+
