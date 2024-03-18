c29856b4eba2ada98a8970e7aef99c3cc07a6a26
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-18 22:17:08 +0530

    Add files via upload

---
+++ 05.typecasting.py
@@ -0,0 +1,11 @@
+# typecasting: convert the datatype of a value to another data type
+
+x = 1  # int
+y = 2.0  # float
+z = "3"  # str
+
+print(x)
+print(int(y))  # it will only help to print the int value of float
+print(int(z)*3)
+
+print("x is: " + str(x))  # here we have to convert int value into str because we can only concatinate string with string so we change datatype of int to str.
