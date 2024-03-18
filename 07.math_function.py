9091103af63df842a1d835ba56133ff733b4e551
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-03-18 22:17:39 +0530

    Add files via upload

---
+++ 07.math_function.py
@@ -0,0 +1,15 @@
+import math
+pi = 3.14
+print(round(pi))  # it will round off the number to 3
+print(math.ceil(pi))  # it will round off the number  to the upper of the value
+print(math.floor(pi))   # it will round off the number to the lowest of the value
+print(abs(pi))  # it will give you the absolute value  which means how far is the value from 0 in positve of negative integer means it will give the same value as the value is .
+print(pow(pi,2))  # it will give you the power of the value
+print(math.sqrt(pi))  # it will give you the square root of the value
+
+
+x = 1
+y = 2
+z = 3
+print(max(x,y,z))  # it will give  you the max value between given value
+print(min(x,y,z))  # it will give  you the min value between given value
